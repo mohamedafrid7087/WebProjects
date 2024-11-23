fd3aad67c75260540dfab6dee1b00718c437c0a2
Mohamed Afrid <meetafrid@gmail.com>
2024-11-23 20:30:22 +0000

    r

--- ExpandingCards/script.js
+++ ExpandingCards/script.js
@@ -2,7 +2,7 @@ const panels = document.querySelectorAll('.panel')
 panels.forEach( panel => {
     panel.addEventListener('click', ()=>{
         removeActiveClasses()
-        panel.classList.add('active')
+        panel.classList.add('active') 
     })
 
 })
